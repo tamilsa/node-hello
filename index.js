dcda1d907e2fe37faa4cdabdd3d0c98f2a9f153a
tamilsa <161282438+tamilsa@users.noreply.github.com>
2025-01-21 21:48:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 6000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello tamil!\n'
+  const msg = 'Hello tamil123!\n'
   res.end(msg);
 });
 
